1bc7376fa69088483f3378c67d62ce9ac74ef37d
Ronel <ronelmelendrez01@gmail.com>
2026-05-14 23:03:41 +0800

    fixed: all the remaining error

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,15 @@
+import { Outlet } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
 import { Sidebar } from '../components/Sidebar';
-import { useAppStore } from '../store/appStore';
 
-export const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  const { isSidebarOpen } = useAppStore();
-
+export const MainLayout = () => {
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
       <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden">
         <Navbar />
         <main className="flex-1 overflow-auto">
-          {children}
+          <Outlet />
         </main>
       </div>
     </div>
